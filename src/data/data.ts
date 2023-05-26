d63e55f55fee417f8a9f258d88f293a5593ea7ba
Michael <massolmichael@gmail.com>
2023-05-26 10:50:24 +0200

    ajout de données dans les tableaux

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,16 +4,22 @@ export const data = [
     {
         id:1,
         title:'avengers',
-        src: "https://th.bing.com/th/id/OIP.nfEMVTdKvwQUi1EXVXi3pAHaK-?pid=ImgDet&rs=1"
+        src: "https://th.bing.com/th/id/OIP.nfEMVTdKvwQUi1EXVXi3pAHaK-?pid=ImgDet&rs=1",
+        userRatings:5,
+        date:'25 april 2012',
     },
     {
         id:2,
         title:'avatar',
-        src: "https://th.bing.com/th/id/R.d9ce72030483939cdc02e059ee98fef0?rik=UFDaj6rx7DCtPQ&pid=ImgRaw&r=0"
+        src: "https://th.bing.com/th/id/R.d9ce72030483939cdc02e059ee98fef0?rik=UFDaj6rx7DCtPQ&pid=ImgRaw&r=0",
+        userRatings:4.5,
+        date:'21 april 2010'
     },
     {
         id:3,
         title:'black adam',
-        src: "https://th.bing.com/th/id/R.0a088c326a53521c6401029d5903e1fe?rik=58BPaq4kdOlWag&pid=ImgRaw&r=0"
+        src: "https://th.bing.com/th/id/R.0a088c326a53521c6401029d5903e1fe?rik=58BPaq4kdOlWag&pid=ImgRaw&r=0",
+        userRatings:4,
+        date:'19 october 2022'
     }
 ];
