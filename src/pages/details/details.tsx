da5333cf120dba258518e7e1656cf70fc8fb419a
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-29 17:08:07 +0200

    add call api

--- src/pages/details/details.tsx
+++ src/pages/details/details.tsx
@@ -1,28 +1,39 @@
-import React from "react";
-import { data } from "../../data/data";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
-    
+import { getMovieById } from '../../api/movie';
+import { Movie } from "../../models/movies";
+
 
 export const Details = () => {
-    
+    const [movieData, setMovieData] = useState<Movie | null>(null);
+    useEffect(() => {
+        const getMovie = async () => {
+            const result = await getMovieById(550);
+            setMovieData(result);
+        }
+        getMovie();
+            
+    }, [movieData])
+
+    if (movieData == null) return null;
     return (
         <main>
             <Link to="/" ><button type="button">Go Back</button></Link>
-            <img src={data[0].poster_path} alt={`poster for the fiml ${data[0].title}`} />
-            <h2>{data[0].title}</h2>
+            <img src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt={`poster for the fiml ${movieData.title}`} />
+            <h2>{movieData.title}</h2>
             <div>
-                <div>
-                    <p>{data[0].popularity}</p>
-                </div>
                 <h3>Ratings</h3>
+                <div>
+                    <p>{movieData.popularity}</p>
+                </div>              
             </div>
             <div>
                 <h3>Release Date</h3>
-                <p>{data[0].release_date}</p>
+                <p>{movieData.release_date}</p>
             </div>
             <div>
                 <h3>Synopsis</h3>
-                <p>{data[0].overview}</p>
+                <p>{movieData.overview}</p>
             </div>
         </main>
     )
