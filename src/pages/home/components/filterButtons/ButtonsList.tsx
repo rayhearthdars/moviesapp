bf26aa495a01f89e68e660ac21072995037c4fc1
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-29 17:10:17 +0200

    add category button to home and api call on buttonslist

--- src/pages/home/components/filterButtons/ButtonsList.tsx
+++ src/pages/home/components/filterButtons/ButtonsList.tsx
@@ -1,18 +1,32 @@
-import React from "react";
-import { genre } from "../../../../data/data";
-import { CategoriesButton } from './Button';
+import React, {useEffect, useState} from "react";
+import { getCategories } from "../../../../api/category";
+import { CategoryButton } from './Button';
+import {Category} from "../../../../models/category"
 import './ButtonsList.css';
 
 
-export const CategoryList = () => {
+export const CategoriesList = () => {
+    const [categoriesData, setCategoriesData] = useState<Category[] | null>(null)
 
+    useEffect(() => {
+        let result;
+        const getCategoriesList = async () => {
+            result = await getCategories();
+                console.log(result);
 
+            setCategoriesData(result.genres);
+        }
+        getCategoriesList();
+    })
+
+    
+    if (categoriesData === null) return null
     return (
-    <>
-        {
-            genre.map(category => (
-                <CategoriesButton key={category.id} name={category.name}  />))
-            }
-    </>
+        <>
+            {
+                categoriesData.map(category => (
+                    <CategoryButton key={category.name} name={category.name}  />))
+                }
+        </>
     );
 }
