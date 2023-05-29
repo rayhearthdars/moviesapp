13d774696b228e821c3bccc7c897ea43b0823591
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-29 11:57:56 +0200

    add routing and remove unnecessary css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+// import { Header } from './components/Header';
+import { Details } from './pages/details/Details';
+import { Home } from './pages/home/Home';
 
 function App() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path=''>
+      <Route path='' element={<Home />} />
+      <Route path='/details' element={<Details />} />
+    </Route>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
