ccd91f03f4a9935f5b376f2c3cbf48a8a3e131d6
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-29 13:57:02 +0200

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,23 +4,14 @@ import { CardsList } from './components/cardsList/cardsList';
 import { data } from '../../data/data';
 import { Header } from '../../components/Header';
 
-const DisplayHeader = () => {return(<Header/>)}
 
-const DisplayCardsList = () => {
-
-    const [cards, setCards] = useState(data);
-    return (
-    <section>
-      <CardsList cards={cards} />
-    </section>
-  );
-  };
 
 export const HomePage = () => {
+    const [cards, setCards] = useState(data);
    return( <body>
     <Header/>
     <main>
-      <DisplayCardsList/>
+      <CardsList cards={cards}/>
     </main>
     </body>);
 }
