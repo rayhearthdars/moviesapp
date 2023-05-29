075905701957a9ebe914c421c8a81ead6a3e5409
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-29 13:50:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 // import { Header } from './components/Header';
 import { Details } from './pages/details/details';
 import { HomePage } from './pages/home/Home';
-import { CardType } from "./models/movie";
 
 function App() {
 
@@ -18,7 +17,6 @@ function App() {
   return (
     <body>
     <RouterProvider router={router}/>
-    <HomePage/>
   </body>);
 }
 
