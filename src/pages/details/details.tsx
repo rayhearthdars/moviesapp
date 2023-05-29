a9c0a3008fba0f148e8d9b11b3588d8fb47c3e41
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-29 11:59:15 +0200

    add details component

--- src/pages/details/details.tsx
+++ src/pages/details/details.tsx
@@ -1,33 +1,28 @@
 import React from "react";
-import {data} from "../../data/data"
+import { data } from "../../data/data";
+import { Link } from "react-router-dom";
     
-type DetailsProps = {
-    src: string;
-    title: string;
-    userRatings: number;
-    date: string;
-    overview: string;
-}
 
 export const Details = () => {
     
     return (
         <main>
-            <img src={data[0].src} alt={`poster for the fiml ${data[0].title}`} />
+            <Link to="../home/Home" ><button type="button">Go Back</button></Link>
+            <img src={data[0].poster_path} alt={`poster for the fiml ${data[0].title}`} />
             <h2>{data[0].title}</h2>
             <div>
                 <div>
-                    <p>{data[0].userRatings}</p>
+                    <p>{data[0].popularity}</p>
                 </div>
                 <h3>Ratings</h3>
             </div>
             <div>
                 <h3>Release Date</h3>
-                <p>{data[0].date}</p>
+                <p>{data[0].release_date}</p>
             </div>
             <div>
                 <h3>Synopsis</h3>
-                <p>{data[0].synopsis}</p>
+                <p>{data[0].overview}</p>
             </div>
         </main>
     )
