9b64bc8dad8b38d70e733d769c7f97d796eb7075
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-31 10:36:18 +0200

    change name

--- src/pages/home/components/cardsList/MoviesList.tsx
+++ src/pages/home/components/cardsList/MoviesList.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Card } from './Movie';
+import React from "react";
+import { Card } from "./Movie";
 import "./MoviesList.css";
-import { Movie } from '../../../../models/movie';
+import { Movie } from "../../../../models/movie";
 
 type CardListProps = {
-  cards:Movie[]
-}
+	cards: Movie[];
+};
 
-export const CardsList = ({cards}:CardListProps) => {
-    return (
-      <section className="card-container">
-        
-          {cards.map((item) => (
-            <Card card={item} key={item.id} />
-          ))}
-        </section>
-      );
-}
+export const CardsList = ({ cards }: CardListProps) => {
+	return (
+		<section className="card-container">
+			{cards.map((item) => (
+				<Card
+					card={item}
+					key={item.id}
+				/>
+			))}
+		</section>
+	);
+};
