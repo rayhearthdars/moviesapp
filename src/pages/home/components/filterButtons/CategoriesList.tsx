ad9f8cb583c5f3191086e98389187d63919da223
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-06-01 11:30:21 +0200

    change movieCard style

--- src/pages/home/components/filterButtons/CategoriesList.tsx
+++ src/pages/home/components/filterButtons/CategoriesList.tsx
@@ -3,7 +3,6 @@ import { getCategories } from "../../../../api/category";
 import { CategoryButton } from "../filterButtons/CategoryButton";
 import { Category } from "../../../../models/category";
 import "./CategoriesList.css";
-import { AllMoviesButton } from "./AllMoviesButton";
 
 
 export const CategoriesList = ({ getCategory }: { getCategory: (category: Category) => void }) => {
